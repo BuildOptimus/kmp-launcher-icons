ea9f36c760d69c7ac483a01775789a4afd480804
FegzDev <fegzdev@gmail.com>
2026-05-27 16:06:45 +0100

    chore: setup project with gradle init

---
+++ plugin/src/main/kotlin/com/buildoptimus/kmp_launcher_icons/KmpLauncherIconsPlugin.kt
@@ -0,0 +1,10 @@
+package com.buildoptimus.kmp_launcher_icons
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class KmpLauncherIconsPlugin: Plugin<Project> {
+    override fun apply(project: Project) {
+
+    }
+}
